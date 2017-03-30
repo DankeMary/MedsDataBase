9a2c8349897ac4481c6b98d90f681366a1a5790e
DankeMary <laktionofs@mail.ru>
2017-03-30 16:16:50 +0300

    Added function "getData"

--- 4_sem_2_cpp/4_sem_2_cpp.cpp
+++ 4_sem_2_cpp/4_sem_2_cpp.cpp
@@ -11,13 +11,87 @@
 
 
 #include "stdafx.h"
+#include <string>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+
+using namespace std;
 
 struct date {
 	int day;
 	int month;
 	int year;
 };
+date getDate() {
+	date d;
+	d.day = 0;
+	d.month = 0;
+	d.year = 0;
+	cout << "Введите день: ";
+	while (d.day < 1 || d.day > 30)
+	{
+		cin >> d.day;
+		if (d.day < 1 || d.day > 30)
+			cout << "Ошибка! Повторите ввод" << endl;
+	}
 
+	cout << "Введите месяц: ";
+	while (d.month < 1 || d.month > 12)
+	{
+		cin >> d.month;
+		if (d.month < 1 || d.month > 30)
+			cout << "Ошибка! Повторите ввод" << endl;
+	}
+
+	cout << "Введите год: ";
+	while (d.year < 1 || d.year > 30)
+	{
+		cin >> d.year;
+		if (d.year < 1900 || d.year > 2020)
+			cout << "Ошибка! Повторите ввод" << endl;
+	}
+	return d;
+}
+class Med {
+private:
+	int farmNum;
+	string name;
+	int quantity;
+	bool available;
+	double price;
+	date arrival;
+	int shelfLife;
+protected:
+	
+	
+	
+public:
+	Med() {
+		farmNum = 0;
+		name = "";
+		quantity = 0;
+		available = false;
+		price = 0.0;
+		arrival.day = 0;
+		arrival.month = 0;
+		arrival.year = 0;
+		shelfLife = 0;
+	}
+	Med(int farmNumTag, string nameTag, int quantTag, bool availTag, double priceTag, date arriveTag, int lifeTag)
+	{
+		farmNum = farmNumTag;
+		name = nameTag;
+		quantity = quantTag;
+		available = availTag;
+		price = priceTag;
+		arrival.day = arriveTag.day;
+		arrival.month = arriveTag.month;
+		arrival.year = arriveTag.year;
+		shelfLife = lifeTag;
+	}
+};
 int main()
 {
     return 0;
